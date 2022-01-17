9c7d5a5c930b55bde5a47c39ef9bfd76d448e524
santiago.hurtado314@gmail.com <santiago.hurtado314@gmail.com>
2022-01-16 20:40:37 -0500

    password generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
 // Assignment code here
-
+function generatePassword()
+{
+  var length = Math.floor(Math.random() * (128 - 8 + 1) + 8),
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*():<>,.?",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i)
+    {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
-function writePassword() {
+function writePassword() 
+{
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,3 +29,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
